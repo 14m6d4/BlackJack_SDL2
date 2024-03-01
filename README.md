4e74ec8cf3ee99c28f60ade956c5d853a02eb3b4
Lambda <tiendat.workspace@gmail.com>
2024-03-01 21:53:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LTNC_SDLGameProject
+ Game project using SDL2 for Advanced Programming (INT2215) course at UET
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include "include/SDL.h"
+using namespace std;
+#undef main
+int main() {
+    SDL_Renderer *renderer;
+    cout << "Hello!";
+}
